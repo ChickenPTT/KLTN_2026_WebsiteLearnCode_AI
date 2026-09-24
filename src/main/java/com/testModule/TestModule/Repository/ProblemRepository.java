b8c5cf943686df05853444115e52a0e388ae9138
Tên của bạn <ngocjthaor1911@gmail.com>
2026-09-24 21:25:21 +0700

    chore(frontend): Remove backend files from Ngoc-FE branch to leave pure frontend codebase

--- src/main/java/com/testModule/TestModule/Repository/ProblemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testModule.TestModule.Repository;
-
-import com.testModule.TestModule.Model.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}
